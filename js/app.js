15475ca26fae8e8a8bbc2e0267847a0266fbd851
AJ (snowlock-dev) <305949454+snowlock-dev@users.noreply.github.com>
2026-08-03 21:51:17 +0530

    fix: increased contrast on muted

--- js/app.js
+++ js/app.js
@@ -105,7 +105,7 @@ window.addEventListener('keydown', (event) => {
 
 // this is more modular, because I plan to add a few more shortcuts later
 window.addEventListener('keydown', (event) => {
-  const isModifierPressed = event.metaKey;
+  const isModifierPressed = event.metaKey || event.ctrlKey;
 
   if (isModifierPressed) {
     const shortcuts = {
